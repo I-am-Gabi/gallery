c522184861c13e0af65db628a4db3c01ecf0b922
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-12-08 03:35:13 -0200

    add django_heroku production

--- gallery/settings/production.py
+++ gallery/settings/production.py
@@ -1,5 +1,5 @@
 from gallery.settings.common import *
-import dj_database_url
+import django_heroku
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -25,6 +25,5 @@ DATABASES = {
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')
-
-db_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_env)
+ 
+django_heroku.settings(locals())
