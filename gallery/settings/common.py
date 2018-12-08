278e343f897e59298323bac2b2a8e44363649c16
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-12-08 16:34:36 -0200

    fix static files

--- gallery/settings/common.py
+++ gallery/settings/common.py
@@ -23,7 +23,7 @@ BASE_DIR = '../' + os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$-f*x%b)4_nq(_odv!rgdn_x)%z-&)&mmf#8o0lw+993#f%tam'
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '.herokuapp.com']
 
 
 # Application definition
