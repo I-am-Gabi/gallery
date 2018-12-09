aa2a3faf442f43672a346dd5d69083747ec0baab
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-12-09 17:26:07 -0200

    add url clean all

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('gallery/approve', views.approve, name='approve'),
     path('gallery/upload_photo', views.upload_photo, name='upload_photo') 
+    path('gallery/clean_all', views.clean_all, name='clean_all') 
 ]
