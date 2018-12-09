29356d1b54263c34b6e65c11b6acbaf19373aa82
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-12-09 17:31:09 -0200

    fix url

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('gallery/approve', views.approve, name='approve'),
-    path('gallery/upload_photo', views.upload_photo, name='upload_photo') 
+    path('gallery/upload_photo', views.upload_photo, name='upload_photo'),
     path('gallery/clean_all', views.clean_all, name='clean_all') 
 ]
